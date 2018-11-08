9fe68a1f78def562dc45d673beb348add649c444
Max Stevenson <maxstevenson@msn.com>
2018-11-08 20:09:58 +0000

    First test for Journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,2 +1,11 @@
 class Journey
+	attr_accessor :complete
+
+	def initialize
+		@complete = false
+	end
+
+	def complete?
+		@complete
+	end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,1 +1,7 @@
 require 'journey'
+
+describe Journey do
+	it 'knows if a journey is not complete' do
+		expect(subject).not_to be_complete
+	end
+end
